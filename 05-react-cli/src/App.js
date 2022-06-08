970ab02d14cfcf3e23ef2806eaec78f1f2e15f58
Matthieu Mota <matthieumota@gmail.com>
2022-06-08 10:58:11 +0200

    People

--- 05-react-cli/src/App.js
+++ 05-react-cli/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Button from './Button';
 import Image from './Image';
+import People from './People';
 
 function App() {
   return (
     <div className="App">
       <Button>Envoyer</Button>
       <Button>Commander</Button>
+      <People />
       <Image src="img/logo.svg" />
     </div>
   );
