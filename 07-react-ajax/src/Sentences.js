669cbdae723b546a892568ee2ac19e2a3634d54f
Matthieu Mota <matthieumota@gmail.com>
2022-06-09 12:06:11 +0200

    Ajout formulaire

--- 07-react-ajax/src/Sentences.js
+++ 07-react-ajax/src/Sentences.js
@@ -5,7 +5,13 @@ import Loader from './Loader';
 class Sentences extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { sentences: [], loading: true, selectedSentence: null };
+        this.state = {
+            sentences: [],
+            loading: true,
+            selectedSentence: null,
+            newSentence: '',
+            newAuthor: '',
+        };
     }
 
     componentDidMount() {
@@ -24,6 +30,31 @@ class Sentences extends React.Component {
             setTimeout(() => this.setState({ selectedSentence: response.data, loading: false }), 200);
         });
         // axios.get(`http://localhost:3001/sentences/${id}`);
+    }
+
+    handleChange = (event) => {
+        // event.target représente le input dans lequel on a saisi un truc
+        console.log(event.target.name);
+        console.log(event.target.value);
+        // [event.target.name]: devient ['newSentence']: devient newSentence:
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    addSentence = () => {
+        // Requête AJAX pour insérer la phrase dans l'API
+        axios.post('http://localhost:3001/sentences', {
+            sentence: this.state.newSentence,
+            author: this.state.newAuthor
+        }).then(response => {
+            console.log(response.data);
+            this.setState({ newSentence: '', newAuthor: '' });
+
+            // Mettre à jour la liste des phrases
+            axios.get('http://localhost:3001/sentences').then(response => {
+                console.log(response.data);
+                this.setState({ sentences: response.data, loading: false });
+            });
+        });
     }
 
     render() {
@@ -50,6 +81,13 @@ class Sentences extends React.Component {
                         </div>
                     )}
                 </div>
+
+                <div className="w-25 mx-auto">
+                    <input type="text" value={this.state.newSentence} onChange={this.handleChange} name="newSentence" className="form-control mb-4" placeholder="Phrase" />
+                    <input type="text" value={this.state.newAuthor} onChange={this.handleChange} name="newAuthor" className="form-control mb-4" placeholder="Auteur" />
+                    <button className="btn btn-primary" onClick={this.addSentence} disabled={!this.state.newSentence || !this.state.newAuthor}>Ajouter</button>
+                </div>
+                Aperçu: {this.state.newSentence} par {this.state.newAuthor}
             </div>
         );
     }
