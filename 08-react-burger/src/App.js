b002930894d777d20ccd816a2a9c7b0c786ff439
Matthieu Mota <matthieumota@gmail.com>
2022-06-09 15:24:50 +0200

    Front burger

--- 08-react-burger/src/App.js
+++ 08-react-burger/src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="burger flex">
+        <img src="https://foodish-api.herokuapp.com/images/burger/burger42.jpg" alt="Normal Burger" />
+        <div className="flex text">
+          <div>
+            <h2>Normal Burger</h2>
+            <p>Un bon burger</p>
+            <a className="btn" href="#">Voir</a>
+          </div>
+          <div className="price">8 €</div>
+        </div>
+      </div>
+      <div className="burger flex">
+        <img src="https://foodish-api.herokuapp.com/images/burger/burger19.jpg" alt="King Burger" />
+        <div className="flex text">
+          <div>
+            <h2>King Burger</h2>
+            <p>Un burger de roi</p>
+            <a className="btn" href="#">Voir</a>
+          </div>
+          <div className="price">10 €</div>
+        </div>
+      </div>
+      <div className="burger flex">
+        <img src="https://foodish-api.herokuapp.com/images/burger/burger18.jpg" alt="Double Burger" />
+        <div className="flex text">
+          <div>
+            <h2>Double Burger</h2>
+            <p>Un burger pour les grandes faims</p>
+            <a className="btn" href="#">Voir</a>
+          </div>
+          <div className="price">12 €</div>
+        </div>
+      </div>
     </div>
   );
 }
