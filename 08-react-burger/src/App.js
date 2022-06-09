f5a47146c4629380ba724ca7127b125c696efb57
Matthieu Mota <matthieumota@gmail.com>
2022-06-09 16:05:59 +0200

    Burgers dans le tableau

--- 08-react-burger/src/App.js
+++ 08-react-burger/src/App.js
@@ -1,41 +1,36 @@
-function App() {
-  return (
-    <div className="container">
-      <div className="burger flex">
-        <img src="https://foodish-api.herokuapp.com/images/burger/burger42.jpg" alt="Normal Burger" />
-        <div className="flex text">
-          <div>
-            <h2>Normal Burger</h2>
-            <p>Un bon burger</p>
-            <a className="btn" href="#">Voir</a>
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      burgers: [
+        { name: 'Normal Burger', description: 'Un bon burger', image: 'https://foodish-api.herokuapp.com/images/burger/burger42.jpg', price: 8 },
+        { name: 'King Burger', description: 'Un burger de roi', image: 'https://foodish-api.herokuapp.com/images/burger/burger19.jpg', price: 10 },
+        { name: 'Double Burger', description: 'Un burger pour les grandes faims', image: 'https://foodish-api.herokuapp.com/images/burger/burger18.jpg', price: 12 }
+      ],
+    }
+  }
+
+  render() {
+    return (
+      <div className="container">
+        {this.state.burgers.map(burger =>
+          <div className="burger flex">
+            <img src={burger.image} alt={burger.name} />
+            <div className="flex text">
+              <div>
+                <h2>{burger.name}</h2>
+                <p>{burger.description}</p>
+                <a className="btn" href="#">Voir</a>
+              </div>
+              <div className="price">{burger.price} €</div>
+            </div>
           </div>
-          <div className="price">8 €</div>
-        </div>
+        )}
       </div>
-      <div className="burger flex">
-        <img src="https://foodish-api.herokuapp.com/images/burger/burger19.jpg" alt="King Burger" />
-        <div className="flex text">
-          <div>
-            <h2>King Burger</h2>
-            <p>Un burger de roi</p>
-            <a className="btn" href="#">Voir</a>
-          </div>
-          <div className="price">10 €</div>
-        </div>
-      </div>
-      <div className="burger flex">
-        <img src="https://foodish-api.herokuapp.com/images/burger/burger18.jpg" alt="Double Burger" />
-        <div className="flex text">
-          <div>
-            <h2>Double Burger</h2>
-            <p>Un burger pour les grandes faims</p>
-            <a className="btn" href="#">Voir</a>
-          </div>
-          <div className="price">12 €</div>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
