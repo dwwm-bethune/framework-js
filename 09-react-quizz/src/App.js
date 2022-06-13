c0931aa603063fcae2d635bff2f5d8c366f0f821
Matthieu Mota <matthieumota@gmail.com>
2022-06-13 08:56:32 +0200

    Quizz

--- 09-react-quizz/src/App.js
+++ 09-react-quizz/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Confetti from 'react-confetti';
 
 class App extends React.Component {
   constructor(props) {
@@ -10,10 +11,41 @@ class App extends React.Component {
         { question: 'Combien font 3 + 1 ?', responses: [5, 4, 3, 10], correct: 1 }
       ],
       currentStep: 0,
+      score: 0,
     };
   }
 
+  nextStep = (response) => {
+    let currentQuestion = this.state.questions[this.state.currentStep];
+    let newScore = this.state.score;
+
+    if (response == currentQuestion.responses[currentQuestion.correct]) {
+      newScore++;
+    }
+
+    this.setState({ currentStep: this.state.currentStep + 1, score: newScore });
+  }
+
+  retry = () => {
+    this.setState({ currentStep: 0, score: 0 });
+  }
+
   render() {
+    // Si l'étape actuelle dépasse la taille du tableau, on affiche l'écran du score
+    if (this.state.currentStep >= this.state.questions.length) {
+      let hasConfetti = (this.state.score * 100 / this.state.questions.length) >= 60;
+
+      return (
+        <div className="app">
+          <div className="score">
+            <h2>Votre score est {this.state.score} / {this.state.questions.length} !</h2>
+            <button className="retry" onClick={this.retry}>Recommencer</button>
+            {hasConfetti && <Confetti />}
+          </div>
+        </div>
+      );
+    }
+
     let currentQuestion = this.state.questions[this.state.currentStep];
 
     return (
@@ -23,7 +55,7 @@ class App extends React.Component {
           <div className="question">
             <h2>{currentQuestion.question}</h2>
             {currentQuestion.responses.map((response, index) =>
-              <button className="response" key={index}>{response}</button>
+              <button className="response" key={index} onClick={() => this.nextStep(response)}>{response}</button>
             )}
           </div>
         </div>
